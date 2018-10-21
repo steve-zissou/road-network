1a5d0b5c2a9e7a436021a893f2be571f4399b05d
Stephen Kitchen <stephen.kitchen-CIC-UK@uk.ibm.com>
2018-10-21 21:40:14 +0100

    Moved node co-ordinate generation out of Node class

--- test.py
+++ test.py
@@ -23,49 +23,50 @@ class Connection:
 class Node:
     """Class to represent a road node."""
 
-    def _get_random_value(self, max):
-        return '{:.2f}'.format(random.uniform(0, max))
-
-    def _generate_node(self, x_max, y_max, z_max):
-        x = self._get_random_value(x_max)
-        y = self._get_random_value(y_max)
-        z = self._get_random_value(z_max)
-        return [x, y, z]
-
     def __str__(self):
         """Get the string representation of the node."""
-        return ','.join([self.x, self.y, self.z])
+        return '{:.2f},{:.2f},{:.2f}'.format(self.x, self.y, self.z)
 
-    def __init__(self, id, x_max=20, y_max=20, z_max=20):
+    def __init__(self, id, x, y, z):
         """Create a Node.
 
         Keyword arguments:
-        x_max -- the maximum x value for a node (default 20)
-        y_max -- the maximum y value for a node (default 20)
-        z_max -- the maximum z value for a node (default 20)
+        id -- the node identifier
+        x -- the x co-ordinate of the node
+        y -- the y co-ordinate of the node
+        z -- the z co-ordinate of the node
         """
         self.id = id
-        self.x, self.y, self.z = self._generate_node(x_max, y_max, z_max)
+        self.x = x
+        self.y = y
+        self.z = z
 
 
 def generate_road_nodes(node_count=5, x_max=20, y_max=20, z_max=20):
     """Generate road nodes of the specified size.
 
     Keyword arguments:
-    node_count -- the number of nodes to create
+    node_count -- the number of nodes to create (default 5)
     x_max -- the maximum x value for a node (default 20)
     y_max -- the maximum y value for a node (default 20)
     z_max -- the maximum z value for a node (default 20)
     """
     nodes = []
     for index, item in enumerate(range(node_count)):
-        nodes.append(Node(index))
+        x = random.uniform(0, x_max)
+        y = random.uniform(0, y_max)
+        z = random.uniform(0, z_max)
+        nodes.append(Node(index, x, y, z))
 
     return nodes
 
 
 def generate_road_connections(nodes, connection_count=5):
     """Generate road connections for the given nodes.
+
+    Keyword arguments:
+    nodes -- the road nodes
+    connection_count -- the number of node connections to create (default 5)
     """
     node_ids = list(map(lambda node: node.id, nodes))
 
@@ -86,7 +87,7 @@ def generate_road_network(node_count=5, x_max=20, y_max=20, z_max=20):
     """Generate a road network of the specified size.
 
     Keyword arguments:
-    node_count -- the real part (default 5)
+    node_count -- the number of nodes to create (default 5)
     x_max -- the maximum x value for a node (default 20)
     y_max -- the maximum y value for a node (default 20)
     z_max -- the maximum z value for a node (default 20)
